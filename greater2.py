194f3c95cdf5ce4ee77eb70459f4b2c346f6e39e
bindumarmaid <42990615+bindumarmaid@users.noreply.github.com>
2018-09-12 10:41:43 +0530

    Add files via upload
    greater than 2 number

---
+++ greater2.py
@@ -0,0 +1,8 @@
+#find greatest of two numbers
+a=int(input("enter the value of a:"))                                                                                                            
+b=int(input("enter the value of b:"))
+if(a>b):
+	print("A is greater than B")
+else:
+	print("B is greater than A")
+    
