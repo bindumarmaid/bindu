b0590d73f218bfd33c39b6b068df9507434c5e29
bindumarmaid <42990615+bindumarmaid@users.noreply.github.com>
2018-09-14 09:06:53 +0530

    equal.py

---
+++ equal.py
@@ -0,0 +1,7 @@
+#write a program to find where the given two numbers are equeal are not
+a=int(input("enter the value of a:"))
+b=int(input("enter the value of b:"))
+if (a==b):
+	print("a is equal to b")
+else:
+	print("a is not equal to b")
