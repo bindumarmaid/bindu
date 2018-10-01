d19a10bc626686316c38b08f320e125091f397ce
bindumarmaid <42990615+bindumarmaid@users.noreply.github.com>
2018-10-01 14:07:10 +0530

    Add files via upload

---
+++ calcy.py
@@ -0,0 +1,22 @@
+#using function
+a=(int(input("enter the value of a")))
+b=(int(input("enter the value of b")))
+
+def add():
+	return a+b
+def sub():
+	return a-b
+def multy():
+	return a*b
+def division():
+	return a/b
+def modulus():
+	return a%b
+print("addition",add())
+print("subtraction",sub())
+print("multiplycation",multy())
+print("division",division())
+print("modulus",modulus())
+
+
+
